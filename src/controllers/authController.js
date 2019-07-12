3663982c11dba123de361b3abccd5cde2bf9c69d
Jose Rodolfo P. Pessanha da Silva <jrodolfo@auxiliadora.censanet.com.br>
2019-07-12 16:45:41 -0300

    Created authentication user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+	return jwt.sign(params, process.env.JWT_SECRET, {
+		expiresIn: 86400 // One day expires
+	});
+}
 
 router.post('/register', async (req, res) => {
 	const { email } = req.body;
@@ -20,10 +28,26 @@ router.post('/register', async (req, res) => {
 		// remove password in output user json
 		user.password = undefined;
 
-		return res.send({ user });
+		return res.send({ user, token: generateToken({ id: user.id }) });
 	} catch (err) {
 		return res.status(400).send({ error: 'Registration Failed.' });
 	}
+});
+
+router.post('/authenticate', async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user) return res.status(400).send({ error: 'User not found.' });
+
+	const isValid = await bcrypt.compare(password, user.password);
+
+	if (!isValid) return res.status(400).send({ error: 'Invalid password.' });
+
+	user.password = undefined;
+
+	res.send({ user, token: generateToken({ id: user.id }) });
 });
 
 module.exports = (app) => app.use('/auth', router);
--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;

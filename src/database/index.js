136e60722010e7f3b51d52b2b2f679c54644cdb7
Jose Rodolfo P. Pessanha da Silva <jrodolfo@auxiliadora.censanet.com.br>
2019-07-12 13:58:55 -0300

    Add connection in database and get data from .env file

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, { useMongoClient: true });
+mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;

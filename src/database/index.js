a81e5eefc502b8b92e2e11eeb1050bfc1111b08c
José Rodolfo <j.rodolfopps@gmail.com>
2019-07-13 10:31:50 -0300

    Updated env variables to access database remote or localhost in settings database file

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, { useNewUrlParser: true });
+if (process.env.DB_REMOTE === "true")
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${
+      process.env.DB_HOST
+    }/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  );
+else
+  mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+    useNewUrlParser: true
+  });
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
